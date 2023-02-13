451ec7d9080299e99546f85e56909224e52fac4c
IgorOliverx <igorferoli019@gmail.com>
2023-02-13 15:15:46 -0300

    primeira parte da aula de 13/02/23

---
+++ CalculadoraDeDescontos/Main.java
@@ -0,0 +1,13 @@
+package CalculadoraDeDescontos;
+
+public class Main {
+    public static void main (String[] args) {
+        CalculadoraDeDescontos cDesconto = new CalculadoraDeDescontos();
+        cDesconto.calcula20();
+        cDesconto.calcula30();
+        cDesconto.calcula50();
+
+
+    }
+    
+}

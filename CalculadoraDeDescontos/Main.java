40fb8fd97d40afe4a42d9a54be05be09fdf238f9
IgorOliverx <igorferoli019@gmail.com>
2023-02-13 16:38:34 -0300

    uso de scanner

--- CalculadoraDeDescontos/Main.java
+++ CalculadoraDeDescontos/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main (String[] args) {
         CalculadoraDeDescontos cDesconto = new CalculadoraDeDescontos();
         cDesconto.calcula20();
-        cDesconto.calcula30();
-        cDesconto.calcula50();
+        
 
 
     }

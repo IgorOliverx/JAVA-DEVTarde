70d02276f73ca6dc81665b312453f21ddf2881c4
IgorOliverx <igorferoli019@gmail.com>
2023-02-13 13:40:10 -0300

    13/02/23

---
+++ AloMundo/teste.java
@@ -0,0 +1,12 @@
+package AloMundo;
+
+public class AloMundo{
+
+public static void main(String[] args) {
+
+    System.out.print("alo mundo")
+
+    
+}
+
+}
---
+++ CalculoVelocidadeMedia/TesteVelMedia.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+import javax.naming.event.NamingExceptionEvent;
+
+/**
+ * TesteVelMedia
+ */
+public class TesteVelMedia {
+
+    public static void main(String[] args) {
+       Scanner s = new Scanner(System.in);
+       System.out.println("Vamos calcular a sua velocidade média?");
+       double b = s.nextDouble();
+       
+
+        
+    }
+}
